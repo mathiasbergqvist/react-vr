0aac4250728cb003883c0b623dd9955d0a464945
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2018-09-23 20:18:31 +0200

    Event handlers to trigger resizing

--- basics/index.vr.js
+++ basics/index.vr.js
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
-import { AppRegistry, asset, Pano, Text, View, Box } from "react-vr";
+import { AppRegistry, asset, Pano, Text, View } from "react-vr";
 
 export default class basics extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      fontSize: 0.1
+    };
+  }
 
   render() {
     return (
       <View>
         <Pano source={asset("starry-sky.jpg")} />
-        <Box
-          dimWidth={0.5}
-          dimHeight={0.5}
-          dimDepth={0.5}
-          wireframe={true}
+        <Text
+          onEnter={() => this.setState({ fontSize: 0.2 })}
+          onExit={() => this.setState({ fontSize: 0.1 })}
           style={{
-            color: '#d22',
+            fontSize: this.state.fontSize,
             transform: [
-              {translate: [0, 0, -3]},
-              {translateY: 1},
-              {translateX: -0.5},
-              {rotateY: 45},
-              {rotateZ: 45}
+              { translate: [0, 0, -2] }
             ]
           }}
-        />
+        >
+          HELLO! HOVER ME!
+    </Text>
       </View>
     );
   }
